57e2c479ef2cec01b9f9a550300a8f843f3f87f7
Serge M <sergii.mysak@vantino.ch>
2022-07-30 18:22:09 +0300

    Add Select color dropdown menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import PositiveButton from "../components/PositiveButton";
 import NegativeButton from "../components/NegativeButton";
 import Group from "../components/Group";
+import DropdownColors from "../components/DropdownColors";
+import {dropdownColors} from "../data/dropdownColors";
+import {useState} from "react";
 
 const Home = () => {
+    const [selected, setSelected] = useState(dropdownColors[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
     return (
         <div>
             <h1>Homepage</h1>
@@ -11,8 +16,28 @@ const Home = () => {
                 <PositiveButton/>
                 <NegativeButton/>
             </Group>
-        </div>
 
+            <br/>
+            <br/>
+
+            <p style={{color: selected.value.toString()}}>
+                Selected color is: {selected.value}
+            </p>
+
+            <button
+                className="ui button"
+                onClick={() => setShowDropdown(!showDropdown)}>
+                Show Dropdown
+            </button>
+
+            {showDropdown ? (
+                <DropdownColors
+                    options={dropdownColors}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            ) : null}
+        </div>
     );
 };
 
