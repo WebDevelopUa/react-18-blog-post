b6e0b41f0c9aed6ae7938c3f559f9d3dbbcd39eb
Serge M <sergii.mysak@vantino.ch>
2022-08-02 16:49:08 +0300

    Add LanguageSelector component

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -3,26 +3,17 @@ import AppHeader from "../components/AppHeader";
 import {Container, Flag} from "semantic-ui-react";
 import {useState} from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import LanguageSelector from "../components/LanguageSelector";
 
 const AppLayout = () => {
     const [language, setLanguage] = useState('netherlandish');
 
-    const onLanguageChange = (lang) => {
-        setLanguage(lang);
-    }
-
     return (
         <Container>
-            <div>
-                Select a Language: &nbsp;
-                <Flag name='netherlands' onClick={() => onLanguageChange('netherlandish')}/>
-                <Flag name='fr' onClick={() => onLanguageChange('french')}/>
-                <Flag name='canada' onClick={() => onLanguageChange('english')}/>
-            </div>
 
-            <p className="ui header centered green">
-                {language.toUpperCase()}
-            </p>
+            <LanguageSelector language={language} setLanguage={setLanguage}/>
+
+            <p className="ui header centered green">{language.toUpperCase()}</p>
 
             <LanguageContext.Provider value={language}>
                 <AppHeader/>
